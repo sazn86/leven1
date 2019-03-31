414184cc777746aeb154c2c329756d9ba3b89a8b
sazn86 <sazn@mail.ru>
2019-03-31 23:24:41 +0300

    Добавил тест файл задания 1

--- level_1.py
+++ level_1.py
@@ -1,11 +1,8 @@
-N = int(input("Введите целое не отрицательное число"))
-def squirrel(z):
+def squirrel(N):
     x = 1
-    for i in range(1,z+1):
+    for i in range(1,N+1):
         x = x * i
-    print (x)
     while x > 0:
         a = x
         x = x // 10
-    print (a)
-squirrel(N)
+    return a
