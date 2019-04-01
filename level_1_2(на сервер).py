33f46879e2ad1c57dc37267edf26c57be8b9336e
Alexz <sazn@mail.ru>
2019-04-01 13:14:52 +0300

    Update level_1_2(на сервер).py

--- level_1_2(на сервер).py
+++ level_1_2(на сервер).py
@@ -1,9 +1,8 @@
-N = [10, 1, 20, 2, 30, 1]
-k = len(N)
 def odometer(N):
+    k = len(N)
     z = 0
     for i in range(k):
         if i % 2 == 0:
             z = z + N[i] * N[i+1]
-    print (z)
+    return z
 odometer(N)
