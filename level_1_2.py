44af11ed5263202e54af2f60e464e193b8b4f3fe
Alexz <sazn@mail.ru>
2019-04-01 15:22:58 +0300

    Время поездки считается - разница

--- level_1_2.py
+++ level_1_2.py
@@ -1,9 +1,11 @@
-N = [10, 1, 20, 2, 30, 1]
+N = [10, 1]
 def odometer(N):
     k = len(N)
     z = 0
     for i in range(k):
-        if i % 2 == 0:
-            z = z + N[i] * N[i+1]
+        if i == 0:
+            z = N[i] * N[i+1]
+        elif i % 2 == 0 and i > 0:
+            z = z + N[i] * (N[i+1]-N[i-1])
     print (z)
 odometer(N)
