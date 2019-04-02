c50f089d30081b2bc9b5e6051fa55b6804673d58
Alexz <sazn@mail.ru>
2019-04-02 13:24:52 +0300

    Update level_1_3.py

--- level_1_3.py
+++ level_1_3.py
@@ -10,8 +10,9 @@ def ConquestCampaign(N, M, L, battalion):
         for a in range(M):
             pole[i].append(1)
     print (pole)
-#Начальные координаты первые удары
+#Начальные координаты - первые удары
     for y in range(0,(L*2),2):
         pole[battalion[y]][battalion[y+1]] = 0
     print (pole)
+
 ConquestCampaign(3,4,2,[1,1,2,3])
