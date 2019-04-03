b6874c030d06db3ac7f0357cf44a11f5b8b499be
Alexz <sazn@mail.ru>
2019-04-03 13:20:15 +0300

    Update level_1_3.py

--- level_1_3.py
+++ level_1_3.py
@@ -1,18 +1,14 @@
-N = 3
-M = 4
-L = 2
-K = [1,1, 2,3]
 def ConquestCampaign(N, M, L, battalion):
     pole = []
 #Создем поле-карту областей и заполняем 1
     for i in range(N):
         pole.append([])
         for a in range(M):
-            pole[i].append(1)
+            pole[i].append(0)
     print (pole)
 #Начальные координаты - первые удары
     for y in range(0,(L*2),2):
-        pole[battalion[y]][battalion[y+1]] = 0
+        pole[battalion[y]][battalion[y+1]] = 1
     print (pole)
 
 ConquestCampaign(3,4,2,[1,1,2,3])
