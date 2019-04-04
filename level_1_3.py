b0ad1d944a177f8befca5328ba0843ed461ac7ff
Alexz <sazn@mail.ru>
2019-04-04 13:19:20 +0300

    Update level_1_3.py

--- level_1_3.py
+++ level_1_3.py
@@ -1,6 +1,12 @@
 def ConquestCampaign(N, M, L, battalion):
     pole = []
-#Создем поле-карту областей и заполняем 1
+#Проверка матрецы на 0
+    def proverka(a,b,c):
+        for i in range(a):
+            for y in range(b):
+                if c[i][y] == 0:
+                    return
+#Создаем поле-карту областей и заполняем 1
     for i in range(N):
         pole.append([])
         for a in range(M):
@@ -9,6 +15,7 @@ def ConquestCampaign(N, M, L, battalion):
 #Начальные координаты - первые удары
     for y in range(0,(L*2),2):
         pole[battalion[y]][battalion[y+1]] = 1
+    proverka(N,M,pole)
     print (pole)
 
 ConquestCampaign(3,4,2,[1,1,2,3])
