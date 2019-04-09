d27c72e533156b83699e0ced6f127ad7fa0a9489
Alexz <sazn@mail.ru>
2019-04-09 12:16:51 +0300

    Update level_1_4.py

--- level_1_4.py
+++ level_1_4.py
@@ -9,4 +9,4 @@ def SynchronizingTables(N, ids, salary):
             if ids[i] == a[j]:
                 c.append(b[j])
     print (c)
-SynchronizingTables (3, [50, 1, 1024], [20000, 100000, 90000])
+SynchronizingTables (10, [1, 3, 243, 234, 4, 5, 6, 9, 121, 123], [21533,20000, 100000, 90000, 123123, 54455, 44464, 5455, 33333, 3434])
