537adb73767fab944d4bd3bb4fc5fa5f81df28be
sazn86 <sazn@mail.ru>
2019-04-10 23:33:09 +0300

    Update level_1_5.py

--- level_1_5.py
+++ level_1_5.py
@@ -12,10 +12,10 @@ def PatternUnlock(N, hits):
                 if hits[(i+1)] == key[j][k]:
                     x2 = j
                     y2 = k
-        if (x2 - x1 + y2 -y1) == 1 or (x2 - x1 + y2 -y1) == 1:
+        if (x2 - x1 + y2 -y1) == 1 or (x2 - x1 + y2 -y1) == -1:
             summa = summa + 1
         else:
             summa = summa + 1.41421
     print (x1,y1,x2,y2)
     print (summa)
-PatternUnlock(9, [1, 2, 3, 4, 5, 6, 2, 7, 8, 9])
+PatternUnlock(10, [1, 2, 3, 4, 5, 6, 2, 7, 8, 9])
