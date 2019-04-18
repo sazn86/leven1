d0c7fe305863299e9aab6e0e1d046f591a32f885
Alexz <sazn@mail.ru>
2019-04-18 16:55:23 +0300

    Update level_1_6.py

--- level_1_6.py
+++ level_1_6.py
@@ -1,3 +1,17 @@
 def WordSearch(len, s, subs):
     x = list(s)
-WordSearch(12, "строка разбивается на набор строк через выравнивание по заданной ширине.", "строк")
+    k = 0
+    y = [][]
+    for i in range((len(x))):
+        for j in range(k,(k+len)):
+            if x[i][j] == ' ':
+                k = j
+            for a in range((k+1)):
+                y.append[i][] = x[i][j]
+
+
+
+
+
+
+WordSearch(12, "1) строка разбивается на набор строк через выравнивание по заданной ширине.", "строк")
