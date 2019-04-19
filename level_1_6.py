7a60bf6457c21b38d0eeac8aea72c06f9992da84
Alexz <sazn@mail.ru>
2019-04-19 14:19:35 +0300

    2

--- level_1_6.py
+++ level_1_6.py
@@ -1,17 +1,17 @@
 def WordSearch(len, s, subs):
     x = list(s)
     k = 0
-    y = [][]
-    for i in range((len(x))):
+    y = []
+    for i in range(7):
+        y.append([])
         for j in range(k,(k+len)):
-            if x[i][j] == ' ':
+            if x[j] == ' ':
                 k = j
-            for a in range((k+1)):
-                y.append[i][] = x[i][j]
-
-
-
-
-
-
+        if x[k] == ' ':
+            for a in range((k+1),(k+2+len)):
+                y[i].append(x[a])
+        else:
+            for a in range((k),(k+1+len)):
+                y[i].append(x[a])
+    print (y)
 WordSearch(12, "1) строка разбивается на набор строк через выравнивание по заданной ширине.", "строк")
--- Для_тесов.py
+++ Для_тесов.py
@@ -1,3 +1,9 @@
-str_a = '5,123,000'
-int_b = int(str_a.replace(',',''))
-print (int_b)
+def WordSearch(len, s, subs):
+    x = list(s)
+    k = 0
+    y = [][]
+    for j in range(k,(k+len)):
+        if x[i][j] == ' ':
+            k += 1
+print (K)
+WordSearch(12, "1) строка разбивается на набор строк через выравнивание по заданной ширине.", "строк")
