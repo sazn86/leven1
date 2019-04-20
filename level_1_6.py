2e45b21632629197ace44c40a37b006d45fc958f
Alexz <sazn@mail.ru>
2019-04-20 13:49:37 +0300

    213

--- level_1_6.py
+++ level_1_6.py
@@ -1,18 +1,31 @@
-def WordSearch(len, s, subs):
+def WordSearch(len1, s, subs):
     x = list(s)
     k = 0
     l = 0
+    b = 0
     y = []
     for i in range(7):
         y.append([])
         z = 0
-        for j in range(l,(l+len)):
+        if (l + len1) <= len(x):
+            b = l + len1
+        else:
+            b = len(x)
+        # Определяем длину одного слотка(списка):
+        for j in range(l,(b)):
             if x[j] == ' ':
                 k = j
                 z = 1
         if z == 0:
-            k = k + len + 1
+            k = k + len1 + 1
+        if k > len(x):
+            k = len(x)
+        # Вносим значения в слот (список):
         if x[k] == ' ':
+            for a in range(l,(k)):
+                y[i].append(x[a])
+            l = k + 1
+        elif x[k+1] == ' ':
             for a in range(l,(k)):
                 y[i].append(x[a])
             l = k + 1
@@ -22,4 +35,4 @@ def WordSearch(len, s, subs):
             l =  k
         print (k)
         print (y)
-WordSearch(12, "1) строка разбивается на набор строк через выравнивание по заданной ширине.", "строк")
+WordSearch(12, "1) строка разбивается на набор строк через выравниван7ие п заданной ширине.", "строк")
