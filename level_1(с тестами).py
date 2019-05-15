77cfbda25cfa1228f524291b114865492cef8bdf
Alexz <sazn@mail.ru>
2019-05-15 16:39:31 +0300

    Update level_1(с тестами).py

--- level_1(с тестами).py
+++ level_1(с тестами).py
@@ -1,13 +1,15 @@
 b = []
 c = []
+l = 0
 def poisk (a):
     b.append([])
+    print (b)
     for i in range(len(a)):
         if len(a[i]) < 2:
             b.append(a[i][0])
         else:
-            b.append()
-            for k in range(len(a[i])):
+            b.append(a[i][0])
+            for k in range(1,len(a[i])):
                 c = []
                 for j in range(len(a)):
                     c.append([])
@@ -16,5 +18,7 @@ def poisk (a):
                 c[i].clear()
                 c[i].append((a[i][k]))
                 print (c)
-            poisk(c)
-poisk([[1],[2,9],[3],[4]])
+                #l = l + 1
+                poisk(c)
+poisk([[1,2],[3],[5],[6,8]])
+print (b)
